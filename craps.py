78dfd2a90bc11cd1b4cebc7c5b1bfe7f016427c2
Stoney Jackson <dr.stoney@gmail.com>
2014-10-01 00:03:27 -0400

    Crappy craps

---
+++ craps.py
@@ -0,0 +1,26 @@
+from random import randint
+
+print('come-out')
+r1 = randint(1, 6)
+r2 = randint(1, 6)
+v = r1 + r2
+print(v)
+if v == 7 or v == 11:
+    print('You win!')
+elif v == 2 or v == 3 or v == 12:
+    print('Craps, you lose.')
+else:
+    print('The point is', v)
+    r1 = randint(1, 6)
+    r2 = randint(1, 6)
+    w = r1 + r2
+    print(w)
+    while w != v and w != 7:
+        r1 = randint(1, 6)
+        r2 = randint(1, 6)
+        w = r1 + r2
+        print(w)
+    if w == 7:
+        print('Seven out, you lose.')
+    else:
+        print('Pass, you win!')
